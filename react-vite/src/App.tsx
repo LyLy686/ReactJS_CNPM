edddc4984b360749e2a03c08664ba82eeb4607ec
Phuong Ly Nguyen <phuonglyvvvv@gmail.com>
2024-03-28 12:20:30 +0700

    first commit

---
+++ react-vite/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Demo from './component/Demo/Demo'
+
+
+function App() {
+  return (
+    <>
+      <Demo/>
+    </>
+  )
+}
+
+export default App
---
+++ react-vite/src/component/Demo/Demo.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+const Demo = ()=>{
+  return(
+    <>
+    <b>Họ và tên</b><p>Nguyễn PHương Ly</p>
+    <b>Họ và tên</b><p>Nguyễn PHương Ly</p>
+    <b>Họ và tên</b><p>Nguyễn PHương Ly</p>
+    </>
+  )
+}
+export default Demo
